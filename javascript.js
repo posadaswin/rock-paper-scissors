12e61b80b620ad78e8dbeebdbe83054bce0627f7
Alpha <crystal.lina1215@gmail.com>
2026-01-12 20:47:25 +0800

    Add player and computer choice
    Create a function to execute player and computer
    choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!")
+function getComputerChoice() {
+    return Math.random();
+};
+
+function getPlayerChoice() {
+    return prompt("Please enter your choice, is it rock, paper, or scissors?");
+}
+
+console.log(getComputerChoice());
+getPlayerChoice();
