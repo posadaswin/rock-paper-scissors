bf20ac0f677b1803bca70cf515f88da3da21deeb
Alpha <crystal.lina1215@gmail.com>
2026-01-12 22:20:00 +0800

    Add game rounds
    Add function that will host the game and determine
    the number of rounds

--- javascript.js
+++ javascript.js
@@ -15,26 +15,42 @@ function choiceConvert(chosenChoice) {
             return a = 0.5;
         case "scissors":
             return a = 0.9;
-    }
+    };
         
 };
 
 function playRound(human, computer) {
+    chosenChoice = getPlayerChoice();
+    humanChoice = choiceConvert(chosenChoice);
+    computerChoice = getComputerChoice();
+
     if (computer > human) {
         console.log("You Lose! Computer beats Human.");
         computerScore++;
     } else {
         console.log("You Win! Human beats Computer.");
         humanScore++;
-    }
+    };
 };
 
-let chosenChoice = getPlayerChoice();
-let humanChoice = choiceConvert(chosenChoice);
-let computerChoice = getComputerChoice();
-let interpret;
+function playGame(session) {
+    for ( let z = 0; z < session; z++) {
+        playRound(humanChoice, computerChoice);
+    };
+    if (humanScore > computerScore) {
+        console.log("The Human is the Winner!!!")
+    } else {
+        console.log("The Computer is the Winner!!!")
+    };
+};
 
+let chosenChoice;
+let humanChoice;
+let computerChoice;
+let interpret;
 let humanScore = 0;
 let computerScore = 0;
+let alpha;
 
-
+alpha = prompt("How many rounds you want?");
+playGame(alpha);
