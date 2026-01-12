8a8d0ef51ac508a55edddda30bf54a3321ec0dca
Alpha <crystal.lina1215@gmail.com>
2026-01-12 22:09:53 +0800

    Add game logic
    Add a function for game logic on how to compare
    human choice to computer choice

--- javascript.js
+++ javascript.js
@@ -3,8 +3,38 @@ function getComputerChoice() {
 };
 
 function getPlayerChoice() {
-    return prompt("Please enter your choice, is it rock, paper, or scissors?");
-}
+    return prompt("Please enter your choice, is it rock, paper, or scissors?").toLowerCase();
+};
 
-console.log(getComputerChoice());
-getPlayerChoice();
+function choiceConvert(chosenChoice) {
+    let a;
+    switch (chosenChoice) {
+        case "rock":
+            return a = 0.1;
+        case "paper":
+            return a = 0.5;
+        case "scissors":
+            return a = 0.9;
+    }
+        
+};
+
+function playRound(human, computer) {
+    if (computer > human) {
+        console.log("You Lose! Computer beats Human.");
+        computerScore++;
+    } else {
+        console.log("You Win! Human beats Computer.");
+        humanScore++;
+    }
+};
+
+let chosenChoice = getPlayerChoice();
+let humanChoice = choiceConvert(chosenChoice);
+let computerChoice = getComputerChoice();
+let interpret;
+
+let humanScore = 0;
+let computerScore = 0;
+
+
