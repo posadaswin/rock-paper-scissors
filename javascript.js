bad98ab30e4b69aed216293239cd254d65fbed86
Alpha <crystal.lina1215@gmail.com>
2026-01-13 18:58:38 +0800

    Separate game logic from round and capitalization
    Separate game logic from game round function
    Capitalize the chosen choice of human
    Add comments

--- javascript.js
+++ javascript.js
@@ -1,13 +1,12 @@
+// declare functions
 
 function getComputerChoice() {
     return Math.random();
 };
 
-
 function getPlayerChoice() {
     return prompt("Please enter your choice, is it rock, paper, or scissors?").toLowerCase();
 };
-
 
 function choiceConvert(chosenChoice) {
     let a;
@@ -18,8 +17,11 @@ function choiceConvert(chosenChoice) {
             return a = 0.5;
         case "scissors":
             return a = 0.9;
-    };
-        
+    };   
+};
+
+function capitalizeFirstLetter(val) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 };
 
 function choiceWin(winner) {
@@ -46,48 +48,59 @@ function choiceDefeat(loser) {
     };
 };
 
-function playRound(human, computer) {
-
-    if (computer > human) {
-        computerHand = choiceWin(chosenChoice);
-        console.log("You Lose! " + computerHand + " beats " + chosenChoice + ".");
-        computerScore++;
-    } else if (human > computer) {
-        computerHand = choiceDefeat(chosenChoice);
-        console.log("You Win! " + chosenChoice + " beats " + computerHand + ".");
-        humanScore++;
-    } else {
-        console.log("It's a tie!");
-    };
-    console.log("Computer Score: " + computerScore + "  " + "Human Score: " + humanScore);
-};
-
-function playGame(session) {
-
-    for ( let z = 0; z < session; z++) {
+function playRound(roundNumber) {
+    for ( let z = 0; z < roundNumber; z++) {
         chosenChoice = getPlayerChoice();
         humanChoice = choiceConvert(chosenChoice);
         computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    };
-    if (humanScore > computerScore) {
-        console.log("The Human is the Winner!!!")
-    } else if (computerScore > humanScore) {
-        console.log("The Computer is the Winner!!!")
-    } else {
-        console.log("It's a tie!")
+        playLogic(humanChoice, computerChoice, chosenChoice);
     };
 };
 
+function playLogic(human, computer, decision) {
+    if (computer > human) {
+        humanHand = capitalizeFirstLetter(decision);
+        computerHand = choiceWin(decision);
+        console.log("You Lose! " + computerHand + " beats " + humanHand + ".");
+        computerScore++;
+    } else if (human > computer) {
+        humanHand = capitalizeFirstLetter(decision);
+        computerHand = choiceDefeat(decision);
+        console.log("You Win! " + humanHand + " beats " + computerHand + ".");
+        humanScore++;
+    } else if (human = computer) {
+        console.log("It's a tie!");
+    } else {
+        return 0;
+    };
+    console.log("Computer Score: " + computerScore + "  " + "Human Score: " + humanScore);
+}
+
+function playGame(session) {
+    playRound(session);
+    if (humanScore > computerScore) {
+        console.log("The Human is the Winner!!!");
+    } else if (computerScore > humanScore) {
+        console.log("The Computer is the Winner!!!");
+    } else if (humanScore = computerScore) {
+        console.log("It's a tie!");
+    } else {
+        return 0;
+    };
+};
+
+// declare global variables
 let chosenChoice;
 let humanChoice;
 let computerChoice;
 let computerHand;
+let humanHand;
 let humanScore = 0;
 let computerScore = 0;
 let alpha;
-let difficulty = 1;
 
+
+// -------------   start of the script   -------------------
 alpha = prompt("How many rounds you want?", 5);
 playGame(alpha);
 
